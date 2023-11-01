356cd19932e36ccade4f8be6b27ec4bb20be208e
tazianeroberta <137331870+tazianeroberta@users.noreply.github.com>
2023-11-01 14:05:45 -0300

    01-11

--- main.js
+++ main.js
@@ -1,39 +1,16 @@
-alert('mulheres solteiras estão a 10km de vocẽ ᕦ( ͡° ͜ʖ ͡°)ᕤ.');
-
-document.querySelector('.tecla_pom').onclick = tocasompom;
-document.querySelector('.tecla_clap').onclick = tocasomclap;
-document.querySelector('.tecla_tim').onclick = tocasomtim;
-document.querySelector('.tecla_puff').onclick = tocasompuff;
-document.querySelector('.tecla_splash').onclick = tocasomsplash;
-document.querySelector('.tecla_toim').onclick = tocasomtoim;
-document.querySelector('.tecla_psh').onclick = tocasompsh;
-document.querySelector('.tecla_tic').onclick = tocasomtic;
-document.querySelector('.tecla_tom').onclick = tocasomtom;
-
-function tocasompom(){
+ 
+ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-function tocasomclap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-function tocasomtim(){
-    document.querySelector('#som_tecla_tim').play();
-}
-function tocasompuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-function tocasomsplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-function tocasomtoim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-function tocasompsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-function tocasomtic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-function tocasomtom(){
-    document.querySelector('#som_tecla_tom').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador <listaDeTeclas.length){
+
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+
+    console.log(contador)
+ 
 }
